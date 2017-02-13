1cdbf6c0bb3a88f70bb011dae12fe28e8338919c
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-13 17:03:37 -0600

    hard mode

--- client.js
+++ client.js
@@ -1,46 +1,23 @@
-console.log('hello');
-var personArray = [
-      {name: 'Tony', yearsOfExperience: 10},
-      {name: 'Carla', yearsOfExperience: 4},
-      {name: 'Kris', yearsOfExperience: 14 }
-    ];
+$(document).ready(function(){
+  var personArray = [
+    {name: 'Tony', yearsOfExperience: 10},
+    {name: 'Carla', yearsOfExperience: 4},
+    {name: 'Kris', yearsOfExperience: 14 }
+  ];
 
-var totalYears = 0;
-// var years = function (array){
-//   (array.forEach(function(totalYears, i){
-//       console.log(totalYears.yearsOfExperience);
-// }
-// });
-
-function years (array){
+  function years (array){
     var totalYears = 0;
     array.forEach(function(employee, i){
       totalYears += employee.yearsOfExperience;
-      console.log(totalYears);
+      $('#employeeTable').append(
+        '<tr>' +
+        '<td>' + employee.name + '</td>' +
+        '<td>' + employee.yearsOfExperience + '</td>' +
+        '<td>' + totalYears + '</td>' +
+        '</tr>'
+      );
     });
     return totalYears;
-}
-
-console.log(years(personArray));
-
-  //
-  //personArray.forEach(function(person, i){ // person means above
-  //   console.log(person.name + ' is at index ' + i);
-  // });
-
-
-    // for (var i = 0; i < personArray.length; i++) {
-    // var person = personArray[i]; // this step is shortened below
-    //    console.log(person.name + ' is at index ' + i);
-    //  }
-
-//   console.log(person.name + ' is at index ' + i);
-// console.log(yourFunction([
-//     { name: Tony, yearsOfExperience: 10 },
-//     { name: Carla, yearsOfExperience: 4 },
-//      { name: Kris, yearsOfExperience: 14 }
-//   ]));
-
-
-//
-// });
+  }
+  console.log(years(personArray));
+});
