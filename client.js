bced1746a39317580dc7409f777271852bf0c982
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-13 19:55:31 -0600

    worked a little longer and get some great help

--- client.js
+++ client.js
@@ -5,9 +5,12 @@ $(document).ready(function(){
     {name: 'Kris', yearsOfExperience: 14 }
   ];
 
+  years(personArray);
+
   function years (array){
     var totalYears = 0;
-    array.forEach(function(employee, i){
+    $('#employeeTable').empty();
+    array.forEach(function(employee){
       totalYears += employee.yearsOfExperience;
       $('#employeeTable').append(
         '<tr>' +
@@ -19,5 +22,25 @@ $(document).ready(function(){
     });
     return totalYears;
   }
-  console.log(years(personArray));
+
+
+  //
+ // event listener click submit button starts a function-
+ // input name, val() back to a var
+ // create a new employee object with key name and our var as populating variable, and
+ // and yearas of Experience key populating with yearsHere variable (both of which we just got from the input fields)
+// then we push this new employee object into the personArray (hard coding?)
+// then we call the years function, passing in personArray as parameter (hard coding?)
+  $('#submit').on('click', function(){
+    var newName = $('#name').val();
+    var yearsHere = parseInt($('#yearsHere').val());
+    var newEmployeeObject = {name: newName, yearsOfExperience: yearsHere};
+    personArray.push(newEmployeeObject);
+    years(personArray);
+  });
 });
+
+// //Pro Mode - New Employee
+// New people are getting added to out company every day. (We're growing!)
+// Create a form that allows new employees to be added to our array
+// and be displayed on the page.
